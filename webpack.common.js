fa0b98d1319daa2385cfaa59be6394a82fdd23a7
Arirzwn <aririzwan0@gmail.com>
2024-11-29 00:16:02 +0700

    update code

--- webpack.common.js
+++ webpack.common.js
@@ -9,6 +9,120 @@ const TerserPlugin = require('terser-webpack-plugin');
 const BundleAnalyzerPlugin =
   require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+const plugins = [
+  new CleanWebpackPlugin({
+    cleanOnceBeforeBuildPatterns: ['**/*', '!images/**'],
+  }),
+  new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: path.resolve(__dirname, 'src/templates/index.html'),
+    cache: true, // Enable caching
+  }),
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: path.resolve(__dirname, 'src/public/'),
+        to: path.resolve(__dirname, 'dist/'),
+        globOptions: {
+          ignore: ['**/images/**'],
+        },
+      },
+      {
+        from: path.resolve(__dirname, 'src/public/images/'),
+        to: path.resolve(__dirname, 'dist/images/'),
+      },
+      {
+        from: path.resolve(__dirname, 'src/public/app.webmanifest'),
+        to: path.resolve(__dirname, 'dist/'),
+      },
+    ],
+  }),
+  new FaviconsWebpackPlugin({
+    logo: path.resolve(__dirname, 'src/public/icons/icon.png'),
+    mode: 'webapp',
+    devMode: 'webapp',
+    favicons: {
+      appName: 'Restaurant Apps',
+      appDescription: 'Restaurant Catalog Application',
+      developerName: 'Ari',
+      developerURL: 'https://github.com/arirzwn',
+      background: '#ffffff',
+      theme_color: '#ffffff',
+      icons: {
+        coast: false,
+        yandex: false,
+      },
+    },
+  }),
+  new GenerateSW({
+    swDest: 'sw.js',
+    clientsClaim: true,
+    skipWaiting: true,
+    runtimeCaching: [
+      {
+        urlPattern: /\.(?:html|css|js)$/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'static-assets',
+        },
+      },
+      {
+        urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'image-assets',
+          expiration: {
+            maxEntries: 30,
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+          },
+        },
+      },
+      {
+        urlPattern: new RegExp('https://restaurant-api.dicoding.dev/list'),
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'restaurant-api',
+          networkTimeoutSeconds: 5,
+          expiration: {
+            maxEntries: 50,
+            maxAgeSeconds: 60 * 60,
+          },
+        },
+      },
+      {
+        urlPattern: new RegExp('https://restaurant-api.dicoding.dev/images/'),
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'restaurant-images',
+          expiration: {
+            maxEntries: 50,
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+      {
+        urlPattern: new RegExp('https://restaurant-api.dicoding.dev/detail/'),
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'restaurant-detail',
+          networkTimeoutSeconds: 5,
+          expiration: {
+            maxEntries: 25,
+            maxAgeSeconds: 7 * 24 * 60 * 60,
+          },
+        },
+      },
+    ],
+  }),
+];
+
+if (process.env.ANALYZE === 'true') {
+  plugins.push(new BundleAnalyzerPlugin());
+}
+
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
@@ -89,114 +203,5 @@ module.exports = {
       }),
     ],
   },
-  plugins: [
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: ['**/*', '!images/**'],
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.resolve(__dirname, 'src/templates/index.html'),
-      cache: true, // Enable caching
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
-          globOptions: {
-            ignore: ['**/images/**'],
-          },
-        },
-        {
-          from: path.resolve(__dirname, 'src/public/images/'),
-          to: path.resolve(__dirname, 'dist/images/'),
-        },
-        {
-          from: path.resolve(__dirname, 'src/public/app.webmanifest'),
-          to: path.resolve(__dirname, 'dist/'),
-        },
-      ],
-    }),
-    new FaviconsWebpackPlugin({
-      logo: path.resolve(__dirname, 'src/public/icons/icon.png'),
-      mode: 'webapp',
-      devMode: 'webapp',
-      favicons: {
-        appName: 'Restaurant Apps',
-        appDescription: 'Restaurant Catalog Application',
-        developerName: 'Ari',
-        developerURL: 'https://github.com/arirzwn',
-        background: '#ffffff',
-        theme_color: '#ffffff',
-        icons: {
-          coast: false,
-          yandex: false,
-        },
-      },
-    }),
-    new GenerateSW({
-      swDest: 'sw.js',
-      clientsClaim: true,
-      skipWaiting: true,
-      runtimeCaching: [
-        {
-          urlPattern: /\.(?:html|css|js)$/,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'static-assets',
-          },
-        },
-        {
-          urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/,
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'image-assets',
-            expiration: {
-              maxEntries: 30,
-              maxAgeSeconds: 30 * 24 * 60 * 60,
-            },
-          },
-        },
-        {
-          urlPattern: new RegExp('https://restaurant-api.dicoding.dev/list'),
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'restaurant-api',
-            networkTimeoutSeconds: 5,
-            expiration: {
-              maxEntries: 50,
-              maxAgeSeconds: 60 * 60,
-            },
-          },
-        },
-        {
-          urlPattern: new RegExp('https://restaurant-api.dicoding.dev/images/'),
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'restaurant-images',
-            expiration: {
-              maxEntries: 50,
-              maxAgeSeconds: 30 * 24 * 60 * 60,
-            },
-            cacheableResponse: {
-              statuses: [0, 200],
-            },
-          },
-        },
-        {
-          urlPattern: new RegExp('https://restaurant-api.dicoding.dev/detail/'),
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'restaurant-detail',
-            networkTimeoutSeconds: 5,
-            expiration: {
-              maxEntries: 25,
-              maxAgeSeconds: 7 * 24 * 60 * 60,
-            },
-          },
-        },
-      ],
-    }),
-    new BundleAnalyzerPlugin(),
-  ],
+  plugins: plugins,
 };
